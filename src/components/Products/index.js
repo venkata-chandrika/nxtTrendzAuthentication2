3b5601648f9be9980e2c0cc71c0984e27f2e5da6
chandrika <balimidivenkatachandrika@gmail.com>
2023-06-01 10:20:59 +0000

    authentication

---
+++ src/components/Products/index.js
@@ -0,0 +1,19 @@
+// Write your JS code here
+import './index.css'
+
+import Header from '../Header'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="product-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+        className="product-img"
+      />
+    </div>
+  </>
+)
+
+export default Products
